f0d31da488508efbb11d916be9f868f591f791a1
Miriam Aguilar <m.aguilarpineda.02@gmail.com>
2024-03-18 16:30:23 -0600

    link de dashboard y presentacion

---
+++ reports/01_link_presentacion.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# URL que quieres abrir
+url = "https://drive.google.com/file/d/1UA0FhkkTn3fLBzEeAyV_71mQ4D8Zu7i3/view?usp=sharing"
+
+# Abre el enlace en el navegador web predeterminado
+webbrowser.open(url)
---
+++ reports/02_link_dashboard.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# URL que quieres abrir
+url = "https://public.tableau.com/views/VirtualPhoneService/VirtualPhoneServiceDashboard?:language=es-ES&publish=yes&:sid=&:display_count=n&:origin=viz_share_link"
+
+# Abre el enlace en el navegador web predeterminado
+webbrowser.open(url)
